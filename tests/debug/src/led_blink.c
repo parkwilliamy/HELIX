34584e71ce1477290350e926ac3699aeaa496155
parkwilliamy <parkwilliamy@gmail.com>
2026-06-22 19:54:41 -0400

    debugging led blink

--- tests/debug/src/led_blink.c
+++ tests/debug/src/led_blink.c
@@ -3,12 +3,13 @@
 int main() {
 
     //unsigned long start = read_csr(mcycle);
-    while (1) {
-        *(volatile int*)0x7800 = 1;
-        while(50000000);
-        *(volatile int*)0x7800 = 0;
-        while(50000000);
-    }
+   
+    while(50000000);
+    *(volatile int*)0x7800 = 1;
+    while(50000000);
+    *(volatile int*)0x7800 = 0;
+    while(50000000);
+    
     //unsigned long elapsed = read_csr(mcycle) - start;
 
     //write_csrs(elapsed);
