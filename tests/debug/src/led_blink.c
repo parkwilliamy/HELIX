4b62f8c40b657b45fd892e7bbb49791491defa20
parkwilliamy <parkwilliamy@gmail.com>
2026-06-22 19:58:59 -0400

    fixed led counter

--- tests/debug/src/led_blink.c
+++ tests/debug/src/led_blink.c
@@ -3,13 +3,15 @@
 int main() {
 
     //unsigned long start = read_csr(mcycle);
-   
-    while(50000000);
-    *(volatile int*)0x7800 = 1;
-    while(50000000);
-    *(volatile int*)0x7800 = 0;
-    while(50000000);
-    
+    int counter = 0;
+    while (1) {
+        counter = 50000000;
+        *(volatile int*)0x7800 = 1;
+        while (counter != 0) {counter -= 1;}
+        *(volatile int*)0x7800 = 0;
+        counter = 50000000;
+        while (counter != 0) {counter -= 1;}
+    }
     //unsigned long elapsed = read_csr(mcycle) - start;
 
     //write_csrs(elapsed);
