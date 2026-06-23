cb394ff6b21d1aa14d3e84ae26bf60ffa32e38da
parkwilliamy <parkwilliamy@gmail.com>
2026-06-22 22:18:17 -0400

    testing loads for io

--- tests/debug/src/led_counter.c
+++ tests/debug/src/led_counter.c
@@ -4,13 +4,15 @@ int main() {
 
     unsigned short tick = 0;
 
-    while (1) {
+    while (tick < 32000) {
 
         *(volatile int*)0x7800 = tick;
-        counter(10000);
+        counter(5000);
         tick++;
       
     }
+
+    *(volatile int*)0x5050 = *((volatile int*)7800);
    
     return 0;
 }
