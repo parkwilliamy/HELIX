d710625a053c1718b5fb688067b6bd23f86dd5c2
parkwilliamy <parkwilliamy@gmail.com>
2026-06-22 20:00:47 -0400

    made counter faster

--- tests/debug/src/led_blink.c
+++ tests/debug/src/led_blink.c
@@ -5,11 +5,11 @@ int main() {
     //unsigned long start = read_csr(mcycle);
     int counter = 0;
     while (1) {
-        counter = 50000000;
+        counter = 10000000;
         *(volatile int*)0x7800 = 1;
         while (counter != 0) {counter -= 1;}
         *(volatile int*)0x7800 = 0;
-        counter = 50000000;
+        counter = 10000000;
         while (counter != 0) {counter -= 1;}
     }
     //unsigned long elapsed = read_csr(mcycle) - start;
