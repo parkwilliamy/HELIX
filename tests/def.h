c9d30b86a161563f3c6bdefc32b80d74f886d0e2
William Park <parkwilliamy@gmail.com>
2026-07-02 21:36:05 -0400

    need to recompile tests

--- tests/def.h
+++ tests/def.h
@@ -1,15 +1,15 @@
 #define RESULT_ADDR ((volatile int*)0x00006000)
-#define CLK_CYCLE_ADDR ((volatile int*)0x00007900)
-#define RETIRED_INSTRUCTIONS_ADDR ((volatile int*)0x00007904)
-#define CORRECT_PREDICTIONS_ADDR ((volatile int*)0x00007908)
-#define TOTAL_PREDICTIONS_ADDR ((volatile int*)0x0000790c)
+#define MCYCLE_ADDR ((volatile int*)0x00007700)
+#define MINSTRET_ADDR ((volatile int*)0x00007908)
+#define CORRECT_PREDICTIONS_ADDR ((volatile int*)0x00007910)
+#define TOTAL_PREDICTIONS_ADDR ((volatile int*)0x00007918)
 
 #define read_csr(reg) ({ unsigned long __v; \
     asm volatile ("csrr %0, " #reg : "=r"(__v)); __v; })
 
 static inline void write_csrs(unsigned long elapsed) {
-    *CLK_CYCLE_ADDR = elapsed;
-    *RETIRED_INSTRUCTIONS_ADDR = read_csr(minstret);
+    *MCYCLE_ADDR = elapsed;
+    *MINSTRET_ADDR = read_csr(minstret);
     *CORRECT_PREDICTIONS_ADDR = read_csr(mhpmcounter3);
     *TOTAL_PREDICTIONS_ADDR = read_csr(mhpmcounter4);
 }
